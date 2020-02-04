2d12e7cda2c07b84ca3c143ea86b57d6ad4d7c2f
Chris Wade <swade781977@yahoo.com>
2020-02-04 02:30:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,6 +19,6 @@ class Cash_Regiter
     a = ".#{a}"
     a.to_i 
     @total = @total - (@total * a)
-    puts "After the discount the total "
+    puts "After the discount, the total comes to #{@total}"
   end
 end
